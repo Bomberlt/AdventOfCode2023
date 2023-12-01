c70f7b433a802a5484b63996c896e7a1d5afafef
PovilasG <pofeliuxxx@gmail.com>
2023-12-01 15:19:23 +0200

    Solution with example data for day1 part 2

--- src/days/Day1.test.tsx
+++ src/days/Day1.test.tsx
@@ -1,4 +1,11 @@
-import { calibrationValue, part1answer } from "./Day1";
+import {
+  calibrationValue,
+  calibrationValuePart2,
+  firstSpelledOutNumber,
+  lastSpelledOutNumber,
+  part1answer,
+  part2answer,
+} from "./Day1";
 
 describe("Day1", () => {
   describe("calibrationValue", () => {
@@ -26,6 +33,68 @@ pqr3stu8vwx
 a1b2c3d4e5f
 treb7uchet`;
       expect(part1answer(input)).toBe(142);
+    });
+  });
+  describe("calibrationValuePart2", () => {
+    it("gets calibration value from line of text two1nine", () => {
+      const lineOfText = "two1nine";
+      expect(calibrationValuePart2(lineOfText)).toBe(29);
+    });
+    it("gets calibration value from line of text eightwothree", () => {
+      const lineOfText = "eightwothree";
+      expect(calibrationValuePart2(lineOfText)).toBe(83);
+    });
+    it("gets calibration value from line of text eightwothree", () => {
+      const lineOfText = "abcone2threexyz";
+      expect(calibrationValuePart2(lineOfText)).toBe(13);
+    });
+  });
+  describe("firstSpelledOutNumber", () => {
+    it("gets index of number", () => {
+      const lineOfText = "two1nine";
+      expect(firstSpelledOutNumber(lineOfText).index).toBe(0);
+    });
+    it("gets value of number", () => {
+      const lineOfText = "two1nine";
+      expect(firstSpelledOutNumber(lineOfText).value).toBe(2);
+    });
+
+    it("gets index of number abcone2threexyz", () => {
+      const lineOfText = "abcone2threexyz";
+      expect(firstSpelledOutNumber(lineOfText).index).toBe(3);
+    });
+  });
+  describe("lastSpelledOutNumber", () => {
+    it("gets index of number", () => {
+      const lineOfText = "two1nine";
+      expect(lastSpelledOutNumber(lineOfText).index).toBe(4);
+    });
+    it("gets value of number", () => {
+      const lineOfText = "two1nine";
+      expect(lastSpelledOutNumber(lineOfText).value).toBe(9);
+    });
+
+    it("gets index of number abcone2threexyz", () => {
+      const lineOfText = "abcone2threexyz";
+      expect(lastSpelledOutNumber(lineOfText).index).toBe(7);
+    });
+
+    it("gets value of number abcone2threexyz", () => {
+      const lineOfText = "abcone2threexyz";
+      expect(lastSpelledOutNumber(lineOfText).value).toBe(3);
+    });
+  });
+
+  describe("part2answer", () => {
+    it("gets part 2 answer", () => {
+      const input = `two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen`;
+      expect(part2answer(input)).toBe(281);
     });
   });
 });
--- src/days/Day1.tsx
+++ src/days/Day1.tsx
@@ -1,17 +1,115 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { input as day1Input } from "../inputs/day1input.ts";
 
 export const calibrationValue = (lineOfText: string): number => {
-  console.log(lineOfText);
   const letters = lineOfText.split("");
   const firstNumber = letters.find((letter) => !isNaN(parseInt(letter)));
   const lastNumber = letters.findLast((letter) => !isNaN(parseInt(letter)));
   return parseInt(`${firstNumber}${lastNumber}`);
 };
 
+enum Digits {
+  one = 1,
+  two,
+  three,
+  four,
+  five,
+  six,
+  seven,
+  eight,
+  nine,
+}
+
+interface SpelledOutNumber {
+  value: number;
+  index: number;
+}
+
+export const firstSpelledOutNumber = (lineOfText: string): SpelledOutNumber => {
+  const one = lineOfText.indexOf(Digits[Digits.one]);
+  const two = lineOfText.indexOf(Digits[Digits.two]);
+  const three = lineOfText.indexOf(Digits[Digits.three]);
+  const four = lineOfText.indexOf(Digits[Digits.four]);
+  const five = lineOfText.indexOf(Digits[Digits.five]);
+  const six = lineOfText.indexOf(Digits[Digits.six]);
+  const seven = lineOfText.indexOf(Digits[Digits.seven]);
+  const eight = lineOfText.indexOf(Digits[Digits.eight]);
+  const nine = lineOfText.indexOf(Digits[Digits.nine]);
+  const arr = [one, two, three, four, five, six, seven, eight, nine];
+  const min = Math.min(
+    one === -1 ? 999 : one,
+    two === -1 ? 999 : two,
+    three === -1 ? 999 : three,
+    four === -1 ? 999 : four,
+    five === -1 ? 999 : five,
+    six === -1 ? 999 : six,
+    seven === -1 ? 999 : seven,
+    eight === -1 ? 999 : eight,
+    nine === -1 ? 999 : nine
+  );
+
+  return {
+    value: arr.findIndex((num) => num === min) + 1,
+    index: min === 999 ? -1 : min,
+  };
+};
+
+// TODO: Rework these two functions into one
+export const lastSpelledOutNumber = (lineOfText: string): SpelledOutNumber => {
+  const one = lineOfText.lastIndexOf(Digits[Digits.one]);
+  const two = lineOfText.lastIndexOf(Digits[Digits.two]);
+  const three = lineOfText.lastIndexOf(Digits[Digits.three]);
+  const four = lineOfText.lastIndexOf(Digits[Digits.four]);
+  const five = lineOfText.lastIndexOf(Digits[Digits.five]);
+  const six = lineOfText.lastIndexOf(Digits[Digits.six]);
+  const seven = lineOfText.lastIndexOf(Digits[Digits.seven]);
+  const eight = lineOfText.lastIndexOf(Digits[Digits.eight]);
+  const nine = lineOfText.lastIndexOf(Digits[Digits.nine]);
+  const arr = [one, two, three, four, five, six, seven, eight, nine];
+  const max = Math.max(one, two, three, four, five, six, seven, eight, nine);
+
+  return {
+    value: arr.findIndex((num) => num === max) + 1,
+    index: max === 999 ? -1 : max,
+  };
+};
+
+export const calibrationValuePart2 = (lineOfText: string): number => {
+  // console.log(lineOfText);
+  const letters = lineOfText.split("");
+  const firstNumberIndex = letters.findIndex(
+    (letter) => !isNaN(parseInt(letter))
+  );
+  const lastNumberIndex = letters.findLastIndex(
+    (letter) => !isNaN(parseInt(letter))
+  );
+
+  const firstTextNumber = firstSpelledOutNumber(lineOfText);
+  const lastTextNumber = lastSpelledOutNumber(lineOfText);
+  const firstNumber =
+    firstNumberIndex !== -1 && firstNumberIndex < firstTextNumber.index
+      ? lineOfText[firstNumberIndex]
+      : firstTextNumber.value;
+  const lastNumber =
+    lastNumberIndex !== -1 && lastNumberIndex > lastTextNumber.index
+      ? lineOfText[lastNumberIndex]
+      : lastTextNumber.value;
+  // console.log("firstNumber", firstNumber);
+  // console.log("lastNumber", lastNumber);
+  return parseInt(`${firstNumber}${lastNumber}`);
+};
+
+export const part2answer = (input: string) => {
+  const lines = input.split("\n");
+  return lines.reduce(
+    (previousValue: number, currentValue: string) =>
+      previousValue + calibrationValuePart2(currentValue),
+    0
+  );
+};
+
 export const part1answer = (input: string) => {
   const lines = input.split("\n");
-  console.log(lines);
   return lines.reduce(
     (previousValue: number, currentValue: string) =>
       previousValue + calibrationValue(currentValue),
@@ -24,6 +122,7 @@ const Day1 = () => {
     <div>
       Day1
       <div>Answer = {part1answer(day1Input)}</div>
+      <div>Part 2 answer = {part2answer(day1Input)}</div>
     </div>
   );
 };
