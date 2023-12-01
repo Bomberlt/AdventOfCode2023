59a61f0cebe8f736c5e79c0ed113492532f3d74a
PovilasG <pofeliuxxx@gmail.com>
2023-12-01 14:09:17 +0200

    Boilerplate for Day1

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,21 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import "./App.css";
 
 import Day12022 from "./days/Day12022.tsx";
+import Day1 from "./days/Day1.tsx";
 
 function App() {
   return (
-    <>
-      <BrowserRouter>
-        <h1>Advent of Code 2023</h1>
-        <div>
-          <h2>Days:</h2>
-          <Link to="/day12022">2022 Day1</Link>
-        </div>
-        <Routes>
-          <Route path="/day12022" element={<Day12022 />} />
-        </Routes>
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <h1>Advent of Code 2023</h1>
+      <div>
+        <h2>Days:</h2>
+        <Link to="/day12022">Day1 (2022)</Link> | <Link to="/day1">Day1</Link>
+      </div>
+      <Routes>
+        <Route path="/day12022" element={<Day12022 />} />
+        <Route path="/day1" element={<Day1 />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/days/Day1.tsx
@@ -0,0 +1,12 @@
+import React, { useEffect } from "react";
+
+const Day1 = () => {
+  return (
+    <div>
+      Day1
+      <div>Answer = ?</div>
+    </div>
+  );
+};
+
+export default Day1;
