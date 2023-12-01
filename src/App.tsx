d37b63a3dab099bb4dd07e686c1406a76b957bb9
PovilasG <pofeliuxxx@gmail.com>
2023-12-01 14:03:17 +0200

    Added 2022 day1 page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import "./App.css";
 
-import Day1 from "./days/Day1.tsx";
+import Day12022 from "./days/Day12022.tsx";
 
 function App() {
   return (
@@ -10,10 +10,10 @@ function App() {
         <h1>Advent of Code 2023</h1>
         <div>
           <h2>Days:</h2>
-          <Link to="/day1">Day1</Link>
+          <Link to="/day12022">2022 Day1</Link>
         </div>
         <Routes>
-          <Route path="/day1" element={<Day1 />} />
+          <Route path="/day12022" element={<Day12022 />} />
         </Routes>
       </BrowserRouter>
     </>
---
+++ src/days/Day12022.tsx
@@ -0,0 +1,43 @@
+import React, { useEffect } from "react";
+
+import { input as day1Input } from "../inputs/day1input.ts";
+
+const elfsCaloriesTotals = (input) => {
+  const allCalories = input
+    .split(`\n\n`)
+    .reduce((acc, elfCalories) => [...acc, elfCalories.split("\n")], []);
+  const firstElfCalories = allCalories[0];
+  console.log("firstElfCalories", firstElfCalories);
+  const totals = allCalories.reduce(
+    (acc, elfCalories) => [
+      ...acc,
+      elfCalories.reduce((acc, calories) => acc + parseInt(calories), 0),
+    ],
+    []
+  );
+  console.log(totals[0]);
+  return totals.sort((a, b) => (a < b ? 1 : -1));
+};
+
+export const day1 = (input) => {
+  const totals = elfsCaloriesTotals(input);
+  return totals[0];
+};
+
+export const day1part2 = (input) => {
+  return elfsCaloriesTotals(input)
+    .slice(0, 3)
+    .reduce((acc, total) => acc + total, 0);
+};
+
+const Day12022 = () => {
+  return (
+    <div>
+      Day1
+      <div>Answer = {day1(day1Input)}</div>
+      <div>Answer part 2 = {day1part2(day1Input)}</div>
+    </div>
+  );
+};
+
+export default Day12022;
