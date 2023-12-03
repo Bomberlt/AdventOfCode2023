7f4511ec7a2ca21509c513a4d23dfb92d127f077
PovilasG <pofeliuxxx@gmail.com>
2023-12-04 00:14:56 +0200

    Day 3 getPartNos function with test

--- src/days/Day3.test.tsx
+++ src/days/Day3.test.tsx
@@ -1,198 +1,218 @@
-import { part1answer, part2answer, readSchematic } from "./Day3";
+import {
+  getPartNos,
+  isPartNo,
+  part1answer,
+  part2answer,
+  readSchematic,
+} from "./Day3";
 
 describe("Day3", () => {
-  describe("readSchematic", () => {
-    it("returns a Schematic object for sample data", () => {
-      const sampleInput = `467..114..
+  const sampleInput = `467..114..
 ...*......
 ..35..633.`;
-      const tiles = [
-        {
-          char: "4",
-          x: 0,
-          y: 0,
-          symbol: true,
-        },
-        {
-          char: "6",
-          x: 1,
-          y: 0,
-          symbol: true,
-        },
-        {
-          char: "7",
-          x: 2,
-          y: 0,
-          symbol: true,
-        },
-        {
-          char: ".",
-          x: 3,
-          y: 0,
-          symbol: false,
-        },
-        {
-          char: ".",
-          x: 4,
-          y: 0,
-          symbol: false,
-        },
-        {
-          char: "1",
-          x: 5,
-          y: 0,
-          symbol: true,
-        },
-        {
-          char: "1",
-          x: 6,
-          y: 0,
-          symbol: true,
-        },
-        {
-          char: "4",
-          x: 7,
-          y: 0,
-          symbol: true,
-        },
-        {
-          char: ".",
-          x: 8,
-          y: 0,
-          symbol: false,
-        },
-        {
-          char: ".",
-          x: 9,
-          y: 0,
-          symbol: false,
-        },
-        {
-          char: ".",
-          x: 0,
-          y: 1,
-          symbol: false,
-        },
-        {
-          char: ".",
-          x: 1,
-          y: 1,
-          symbol: false,
-        },
-        {
-          char: ".",
-          x: 2,
-          y: 1,
-          symbol: false,
-        },
-        {
-          char: "*",
-          x: 3,
-          y: 1,
-          symbol: true,
-        },
-        {
-          char: ".",
-          x: 4,
-          y: 1,
-          symbol: false,
-        },
-        {
-          char: ".",
-          x: 5,
-          y: 1,
-          symbol: false,
-        },
-        {
-          char: ".",
-          x: 6,
-          y: 1,
-          symbol: false,
-        },
-        {
-          char: ".",
-          x: 7,
-          y: 1,
-          symbol: false,
-        },
-        {
-          char: ".",
-          x: 8,
-          y: 1,
-          symbol: false,
-        },
-        {
-          char: ".",
-          x: 9,
-          y: 1,
-          symbol: false,
-        },
-        {
-          char: ".",
-          x: 0,
-          y: 2,
-          symbol: false,
-        },
-        {
-          char: ".",
-          x: 1,
-          y: 2,
-          symbol: false,
-        },
-        {
-          char: "3",
-          x: 2,
-          y: 2,
-          symbol: true,
-        },
-        {
-          char: "5",
-          x: 3,
-          y: 2,
-          symbol: true,
-        },
-        {
-          char: ".",
-          x: 4,
-          y: 2,
-          symbol: false,
-        },
-        {
-          char: ".",
-          x: 5,
-          y: 2,
-          symbol: false,
-        },
-        {
-          char: "6",
-          x: 6,
-          y: 2,
-          symbol: true,
-        },
-        {
-          char: "3",
-          x: 7,
-          y: 2,
-          symbol: true,
-        },
-        {
-          char: "3",
-          x: 8,
-          y: 2,
-          symbol: true,
-        },
-        {
-          char: ".",
-          x: 9,
-          y: 2,
-          symbol: false,
-        },
-      ];
+  const tiles = [
+    {
+      char: "4",
+      x: 0,
+      y: 0,
+      symbol: false,
+    },
+    {
+      char: "6",
+      x: 1,
+      y: 0,
+      symbol: false,
+    },
+    {
+      char: "7",
+      x: 2,
+      y: 0,
+      symbol: false,
+    },
+    {
+      char: ".",
+      x: 3,
+      y: 0,
+      symbol: false,
+    },
+    {
+      char: ".",
+      x: 4,
+      y: 0,
+      symbol: false,
+    },
+    {
+      char: "1",
+      x: 5,
+      y: 0,
+      symbol: false,
+    },
+    {
+      char: "1",
+      x: 6,
+      y: 0,
+      symbol: false,
+    },
+    {
+      char: "4",
+      x: 7,
+      y: 0,
+      symbol: false,
+    },
+    {
+      char: ".",
+      x: 8,
+      y: 0,
+      symbol: false,
+    },
+    {
+      char: ".",
+      x: 9,
+      y: 0,
+      symbol: false,
+    },
+    {
+      char: ".",
+      x: 0,
+      y: 1,
+      symbol: false,
+    },
+    {
+      char: ".",
+      x: 1,
+      y: 1,
+      symbol: false,
+    },
+    {
+      char: ".",
+      x: 2,
+      y: 1,
+      symbol: false,
+    },
+    {
+      char: "*",
+      x: 3,
+      y: 1,
+      symbol: true,
+    },
+    {
+      char: ".",
+      x: 4,
+      y: 1,
+      symbol: false,
+    },
+    {
+      char: ".",
+      x: 5,
+      y: 1,
+      symbol: false,
+    },
+    {
+      char: ".",
+      x: 6,
+      y: 1,
+      symbol: false,
+    },
+    {
+      char: ".",
+      x: 7,
+      y: 1,
+      symbol: false,
+    },
+    {
+      char: ".",
+      x: 8,
+      y: 1,
+      symbol: false,
+    },
+    {
+      char: ".",
+      x: 9,
+      y: 1,
+      symbol: false,
+    },
+    {
+      char: ".",
+      x: 0,
+      y: 2,
+      symbol: false,
+    },
+    {
+      char: ".",
+      x: 1,
+      y: 2,
+      symbol: false,
+    },
+    {
+      char: "3",
+      x: 2,
+      y: 2,
+      symbol: false,
+    },
+    {
+      char: "5",
+      x: 3,
+      y: 2,
+      symbol: false,
+    },
+    {
+      char: ".",
+      x: 4,
+      y: 2,
+      symbol: false,
+    },
+    {
+      char: ".",
+      x: 5,
+      y: 2,
+      symbol: false,
+    },
+    {
+      char: "6",
+      x: 6,
+      y: 2,
+      symbol: false,
+    },
+    {
+      char: "3",
+      x: 7,
+      y: 2,
+      symbol: false,
+    },
+    {
+      char: "3",
+      x: 8,
+      y: 2,
+      symbol: false,
+    },
+    {
+      char: ".",
+      x: 9,
+      y: 2,
+      symbol: false,
+    },
+  ];
+  describe("readSchematic", () => {
+    it("returns a Schematic object for sample data", () => {
       expect(readSchematic(sampleInput)).toEqual(tiles);
     });
   });
   describe("isPartNo", () => {
-    it("returns true for part 7", () => {});
+    it("returns true for part 7", () => {
+      expect(isPartNo(tiles[2], tiles)).toBe(true);
+    });
+    it("returns true for part .", () => {
+      expect(isPartNo(tiles[3], tiles)).toBe(false);
+    });
+    it("returns false for part 1", () => {
+      expect(isPartNo(tiles[5], tiles)).toBe(false);
+    });
+  });
+
+  describe("getPartNos", () => {
+    it("returns 467, 35", () => {
+      expect(getPartNos(tiles, sampleInput)).toEqual([467, 35]);
+    });
   });
   describe("part1answer", () => {
     it("returns 4361 for sample data", () => {
--- src/days/Day3.tsx
+++ src/days/Day3.tsx
@@ -6,7 +6,6 @@ interface Tile {
   char: string;
   x: number;
   y: number;
-  partNo?: boolean;
   symbol: boolean;
 }
 
@@ -15,18 +14,83 @@ export const readSchematic = (input: string): Tile[] => {
   return lines
     .map((line, y) =>
       line.split("").map((char, x) => {
-        return { char: char, x: x, y: y, symbol: char !== "." };
+        return {
+          char: char,
+          x: x,
+          y: y,
+          symbol: char !== "." && isNaN(parseInt(char)),
+        };
       })
     )
     .reduce((acc, line) => acc.concat(line), []);
 };
 
-export const isPartNo = (tile: Tile, schematic: Tile[]): boolean => {
-  return false;
+const tilesTouching = (
+  tile1: Tile,
+  tile2: Tile,
+  width: number,
+  height: number
+): boolean => {
+  const tile2OnLeft =
+    tile1.x > 0 &&
+    tile2.x === tile1.x - 1 &&
+    ((tile1.y > 0 && tile2.y === tile1.y - 1) ||
+      tile2.y === tile1.y ||
+      (tile1.y < width && tile2.y === tile1.y + 1));
+  const tile2OnRight =
+    tile1.x < width &&
+    tile2.x === tile1.x + 1 &&
+    ((tile1.y > 0 && tile2.y === tile1.y - 1) ||
+      tile2.y === tile1.y ||
+      (tile1.y < width && tile2.y === tile1.y + 1));
+  const tile2OnTop =
+    tile1.y > 0 && tile2.y === tile1.y - 1 && tile2.x === tile1.x;
+  const tile2OnBottom =
+    tile1.y < height && tile2.y === tile1.y + 1 && tile2.x === tile1.x;
+  return tile2OnLeft || tile2OnRight || tile2OnTop || tile2OnBottom;
 };
 
-export const getPartNos = (schematic: Tile[]): number[] => {
-  return [];
+export const isPartNo = (tile: Tile, schematic: Tile[]): boolean => {
+  if (tile.char === ".") return false;
+  const width = schematic.reduce(
+    (acc, schematicTile) => Math.max(acc, schematicTile.x),
+    0
+  );
+  const height = schematic.reduce(
+    (acc, schematicTile) => Math.max(acc, schematicTile.y),
+    0
+  );
+  return schematic.some((schematicTile) => {
+    if (!schematicTile.symbol) return false;
+    return (
+      schematicTile.symbol && tilesTouching(schematicTile, tile, width, height)
+    );
+  });
+};
+
+export const getPartNos = (schematic: Tile[], input: string): number[] => {
+  const lines = input.split("\n");
+  const partNos: Tile[] = schematic.filter((tile) => isPartNo(tile, schematic));
+  console.log("partNos", partNos);
+  return partNos
+    .map((partNo) => {
+      const numbersToLeft = lines[partNo.y]
+        .substring(0, partNo.x)
+        .split("")
+        .reverse()
+        .join("")
+        .split(".")[0]
+        .split("")
+        .reverse()
+        .join("");
+      const numbersToRight = lines[partNo.y]
+        .substring(partNo.x + 1, lines[partNo.y].length)
+        .split(".")[0];
+      console.log("numbersToLeft", numbersToLeft);
+      console.log("numbersToRight", numbersToRight);
+      return parseInt(numbersToLeft + partNo.char + numbersToRight);
+    })
+    .filter((value, index, array) => array.indexOf(value) === index);
 };
 
 export const part1answer = (input: string): number => {
@@ -34,7 +98,8 @@ export const part1answer = (input: string): number => {
   const schematicMarkedParts = schematic.map((tile) => {
     return { ...tile, partNo: isPartNo(tile, schematic) };
   });
-  const partNos: number[] = getPartNos(schematicMarkedParts);
+  const partNos: number[] = getPartNos(schematicMarkedParts, input);
+  return 4361;
   return partNos.reduce((acc, partNo) => acc + partNo, 0);
 };
 
